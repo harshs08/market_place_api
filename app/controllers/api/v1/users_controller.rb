1951d0e49a5cc6a335c0ea3b5047234d6a8ce887
Harsh Singh <harshsin@usc.edu>
2015-11-16 00:06:11 -0800

    Adds update action the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,7 +8,18 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user, status: 201, location: [:api, :user]
+      render json: user, status: 201, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+    user = User.find(params[:id])
+      render json: user, status: 200, location: [:api, user]
     else
       render json: { errors: user.errors }, status: 422
     end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -21,7 +21,6 @@ describe Api::V1::UsersController do
   end
 
   describe "POST #create" do
-
     context "when is successfully created" do
       let(:user_attributes) { FactoryGirl.attributes_for :user }
       before(:each) do
@@ -55,7 +54,46 @@ describe Api::V1::UsersController do
 
       it { is_expected.to respond_with 422 }
     end
+  end
 
+  describe "PUT/PATCH #update" do
+
+    context "when is successfully updated" do
+
+      let(:user) { FactoryGirl.create :user }
+      before(:each) do
+        patch :update, { id: user.id,
+                         user: { email: "newmail@example.com" } }, format: :json
+      end
+
+      it "renders the json representation for the updated user" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql "newmail@example.com"
+      end
+
+      it { is_expected.to respond_with 200 }
+    end
+
+    context "when is not created" do
+
+      let(:user) { FactoryGirl.create :user }
+      before(:each) do
+        patch :update, { id: user.id,
+                         user: { email: "bademail.com" } }, format: :json
+      end
+
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on whye the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
+
+      it { is_expected.to respond_with 422 }
+    end
   end
 
 end
