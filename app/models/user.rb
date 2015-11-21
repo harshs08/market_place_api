6f84faa35283e22e02b7751b360a649b6f333262
Harsh Singh <harshsin@usc.edu>
2015-11-21 15:19:40 -0800

    Adds sessions controller create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
+  validates :auth_token, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :auth_token, uniqueness: true
 end
