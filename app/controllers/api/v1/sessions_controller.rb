f9f2c231be1535e22a30d17822d6c9dde9703b64
Harsh Singh <harshsin@usc.edu>
2015-11-21 15:47:23 -0800

    Fixes the session controller typo

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     user_password = params[:session][:password]
-    user_email    = params[:session][:password]
-    user = user_email.present? && User.find_by(email: user_email)
+    user_email    = params[:session][:email]
+    user          = user_email.present? && User.find_by(email: user_email)
 
     if user.valid_password? user_password
       sign_in user, store: false
@@ -11,7 +11,7 @@ class Api::V1::SessionsController < ApplicationController
       user.save
       render json: user, status: 200, location: [:api, user]
     else
-      render json: { errors: "Invalid email or password"}, status: 422
+      render json: { errors: "Invalid email or password" }, status: 422
     end
   end
 
