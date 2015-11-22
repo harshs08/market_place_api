c48ae7742a447a659981f2c85aaa9fa06e3bda2f
Harsh Singh <harshsin@usc.edu>
2015-11-21 16:09:31 -0800

    Adds destroy session action added

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -32,4 +32,16 @@ describe Api::V1::SessionsController do
 
     end
   end
+
+  describe "DELETE #destroy" do
+
+    let(:user) { FactoryGirl.create :user }
+
+    before(:each) do
+      sign_in user
+      delete :destroy, id: user.auth_token
+    end
+    it { is_expected.to respond_with 204 }
+
+  end
 end
