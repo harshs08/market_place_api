15cf0218d68b777560edc73b614ec30386b1cf3e
Harsh Singh <harshsin@usc.edu>
2015-12-08 01:30:20 -0800

    Adds the products create, update and destroy action nested on the user resources

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ MarketPlaceApi::Application.routes.draw do
     scope module: :v1,
                   constraints: ApiConstraints.new(version: 1, default: true) do
 
-      resources :users, :only => [:show, :create, :update, :destroy]
+      resources :users, :only => [:show, :create, :update, :destroy] do
+         resources :products, :only => [:create, :update, :destroy]
+      end
       resources :sessions, :only => [:create, :destroy]
       resources :products, :only => [:show, :index]
     end 
--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -17,9 +17,6 @@ describe Api::V1::ProductsController do
   end
 
   describe "GET #index" do
-    # let(:products) do
-    #   4.times { FactoryGirl.create :product }
-    # end
     before(:each) do
       4.times { FactoryGirl.create :product }
       get :index
@@ -31,6 +28,98 @@ describe Api::V1::ProductsController do
     end
 
     it { is_expected.to respond_with 200 }
+  end
+
+  describe "POST #create" do
+    context "when is successfully created" do
+      let(:user) { FactoryGirl.create :user }
+      let(:product_attributes) { FactoryGirl.attributes_for :product }
+      before(:each) do
+        api_authorization_header user.auth_token
+        post :create, { user_id: user.id, product: product_attributes }
+      end
+
+      it "renders the json representation for the product record just created" do
+        product_response = json_response
+        expect(product_response[:title]).to eql product_attributes[:title]
+      end
+
+      it { is_expected.to respond_with 201 }
+    end
+
+    context "when is not created" do
+      let(:user) { FactoryGirl.create :user }
+
+      before(:each) do
+        @invalid_product_attributes = { title: "Smart TV", price: "Twelve dollars" }
+        api_authorization_header user.auth_token
+        post :create, { user_id: user.id, product: @invalid_product_attributes }
+      end
+
+      it "renders an errors json" do
+        product_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on whye the user could not be created" do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include "is not a number"
+      end
+
+      it { is_expected.to respond_with 422 }
+    end
+  end
+
+  describe "PUT/PATCH #update" do
+    let(:user) { FactoryGirl.create :user }
+    let(:product) { FactoryGirl.create :product, user: user }
+    before(:each) do
+      api_authorization_header user.auth_token
+    end
+
+    context "when is successfully updated" do
+      before(:each) do
+        patch :update, { user_id: user.id, id: product.id,
+              product: { title: "An expensive TV" } }
+      end
+
+      it "renders the json representation for the updated user" do
+        product_response = json_response
+        expect(product_response[:title]).to eql "An expensive TV"
+      end
+
+      it { is_expected.to respond_with 200 }
+    end
+
+    context "is not updated" do
+      before(:each) do
+        patch :update, {user_id: user.id, id: product.id,
+          product: { price: "two hundred" }}
+      end
+
+      it "renders an errors json" do
+        product_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on whye the user could not be created" do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include "is not a number"
+      end
+
+      it { is_expected.to respond_with 422 }
+    end
+  end
+
+  describe "DELETE #destroy" do
+    let(:user) { FactoryGirl.create :user }
+    let(:product) { FactoryGirl.create :product, user: user }
+    before(:each) do
+      api_authorization_header user.auth_token
+      delete :destroy, { user_id: user.id, id: product.id }
+    end
+
+    it { is_expected.to respond_with 204 }
   end
 
 end
