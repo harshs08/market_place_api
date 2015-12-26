561912de3b6bd9a7c6cca384e543617e4d18c67c
Harsh Singh <harshsin@usc.edu>
2015-12-25 20:50:07 -0800

    Decrements the product quantity by the placement quantity

--- spec/models/placement_spec.rb
+++ spec/models/placement_spec.rb
@@ -3,12 +3,19 @@ require 'spec_helper'
 describe Placement do
   let(:product) { FactoryGirl.build :product }
   let(:order)   { FactoryGirl.build :order }
-  let(:placement) { FactoryGirl.build :placement, product_id: product.id, order_id: order.id }
+  let(:placement) { FactoryGirl.build :placement, product: product, order_id: order }
   subject { placement }
 
   it { is_expected.to respond_to :order_id }
   it { is_expected.to respond_to :product_id }
   it { is_expected.to respond_to :quantity }
+
+  describe "#decrement_product_quantity!" do
+    it "decreases the product quantity by the placement quantity" do
+      product = placement.product
+      expect{placement.decrement_product_quantity!}.to change{product.quantity}.by(-placement.quantity)
+    end
+  end
 
   it { is_expected.to belong_to :order }
   it { is_expected.to belong_to :product }
